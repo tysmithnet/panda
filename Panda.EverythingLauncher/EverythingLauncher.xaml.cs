0b2d9795a8158cec9e9a3d104ae6ec76fcfb2a3c
tysmithnet <dts5004@gmail.com>
2017-12-23 15:53:47 -0800

    Added EverythingLauncher project

---
+++ Panda.EverythingLauncher/EverythingLauncher.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Panda.Client;
+
+namespace Panda.EverythingLauncher
+{
+    /// <summary>
+    /// Interaction logic for EverythingLauncher.xaml
+    /// </summary>
+    public partial class EverythingLauncher : Launcher
+    {
+        public EverythingLauncher()
+        {
+            InitializeComponent();
+        }
+    }
+}
