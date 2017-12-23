3b6ba997ab2a636a4097235e879eca676e777127
tysmithnet <dts5004@gmail.com>
2017-12-22 23:09:14 -0800

    Added Hello World

--- Panda.Client/App.xaml.cs
+++ Panda.Client/App.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,16 @@ namespace Panda.Client
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var assemblyPaths = Directory.EnumerateFiles(assemblyPath, "Panda.*.dll");
+            var catalogs = assemblyPaths.Select(a => new AssemblyCatalog(a));
+            var aggregateCatalog = new AggregateCatalog(catalogs);
+            var compositionContainer = new CompositionContainer(aggregateCatalog);
+
+            var launchers = compositionContainer.GetExportedValues<Launcher>().First();
+            launchers.Show();
+        }
     }
 }
