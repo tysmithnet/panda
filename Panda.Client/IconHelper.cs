2bd4f1d2a3474743dfd9ceedf09126a15152b43c
Ty Smith <dts5004@gmail.com>
2018-01-01 01:39:50 -0800

    Made IconHelper fallback to unknown file icon https://github.com/tysmithnet/panda/issues/8

--- Panda.Client/IconHelper.cs
+++ Panda.Client/IconHelper.cs
@@ -1,30 +1,73 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Panda.Client
-{
+{         
     /// <summary>
     ///     Class that assists with the loading of icons
     /// </summary>
     public static class IconHelper
     {
         /// <summary>
+        /// Win32 Api Shell File Info structure
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        struct ShFileInfo
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        };
+
+        /// <summary>
+        /// Local win32 api wrapper
+        /// </summary>
+        class Win32
+        {
+            public const uint SHGFI_ICON = 0x100;
+            public const uint SHGFI_LARGEICON = 0x0; // 'Large icon
+            public const uint SHGFI_SMALLICON = 0x1; // 'Small icon
+
+            [DllImport("shell32.dll")]
+            public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref ShFileInfo psfi, uint cbSizeFileInfo, uint uFlags);
+        }
+
+        /// <summary>
         ///     Gets an icon from a file path using windows shell
         /// </summary>
         /// <param name="filePath">The file path.</param>
         /// <returns>ImageSource for the icon for the icon at filePath</returns>
-        // todo: add exception handling
         public static ImageSource IconFromFilePath(string filePath)
         {
-            var img = Imaging.CreateBitmapSourceFromHIcon(
-                Icon.ExtractAssociatedIcon(filePath).Handle,
-                Int32Rect.Empty,
-                BitmapSizeOptions.FromEmptyOptions());
-            img.Freeze();
-            return img;
+            try
+            {              
+                var shinfo = new ShFileInfo();
+                var hImgSmall = Win32.SHGetFileInfo(filePath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), Win32.SHGFI_ICON | Win32.SHGFI_LARGEICON); // todo: handle big/small requests
+                var icon = (Icon)Icon.FromHandle(shinfo.hIcon).Clone();
+                var bmp = icon.ToBitmap();
+                var img = Imaging.CreateBitmapSourceFromHBitmap(bmp.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+                img.Freeze();
+                return img;
+            }
+            catch (Exception)
+            {   
+                Icon icon = new Icon("unknownfile32x32.ico");
+                var bmp = icon.ToBitmap();
+                var img = Imaging.CreateBitmapSourceFromHBitmap(bmp.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+                img.Freeze();
+                return img;
+            }
         }
     }
 }
