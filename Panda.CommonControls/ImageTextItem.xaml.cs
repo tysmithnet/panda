636aeed601994daf87d44aca5ab70c3ad8db0ae0
Ty Smith <dts5004@gmail.com>
2018-01-03 01:53:00 -0800

    Successfully extracted ImageTextItem to reusable component

--- Panda.CommonControls/ImageTextItem.xaml.cs
+++ Panda.CommonControls/ImageTextItem.xaml.cs
@@ -42,132 +42,27 @@ namespace Panda.CommonControls
         }
         public static readonly DependencyProperty SubHeaderTextProperty = DependencyProperty.Register("SubHeaderText", typeof(string), typeof(ImageTextItem));
 
-        public string ImageSource
+        public ImageSource ImageSource
         {
-            get => base.GetValue(ImageSourceProperty) as string;
+            get => base.GetValue(ImageSourceProperty) as ImageSource;
             set => base.SetValue(ImageSourceProperty, value);
         }
-        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(string), typeof(ImageTextItem));
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ImageTextItem));
 
         public ImageTextItem()
         {
             InitializeComponent();
-            ViewModel = new ImageTextItemViewModel
-            {
-                   ImageSourceChangedObs = ImageSourceChangedSubject,
-                   HeaderTextChangedObs = HeaderTextChangedSubject,
-                   SubHeaderTextChangedObs = SubHeaderChangedSubject
-            };
-            DataContext = ViewModel;
+            LayoutRoot.DataContext = this;
         }
-
-        public ImageTextItemViewModel ViewModel { get; set; }
-
+                                                                     
         private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            HeaderTextChangedSubject.OnNext(HeaderTextBox.Text);
+            //HeaderTextChangedSubject.OnNext(HeaderTextBox.Text);
         }
 
         private void SubHeaderText_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            SubHeaderChangedSubject.OnNext(SubHeaderTextBox.Text);
+            //SubHeaderChangedSubject.OnNext(SubHeaderTextBox.Text);
         }
-    }
-
-    public class ImageTextItemViewModel : INotifyPropertyChanged
-    {
-        private IObservable<ImageSource> _imageSourceChangedObs;
-
-        public ImageSource ImageSource
-        {
-            get => _imageSource;
-            set
-            {
-                _imageSource = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string HeaderText
-        {
-            get => _headerText;
-            set
-            {
-                _headerText = value; 
-                OnPropertyChanged();
-            }
-        }
-
-        public string SubHeaderText
-        {
-            get => _subHeaderText;
-            set
-            {
-                _subHeaderText = value; 
-                OnPropertyChanged();
-            }
-        }
-
-        private IDisposable _imageSourceChangedSubscription;
-        private IObservable<string> _headerTextChangedObs;
-
-        public IObservable<ImageSource> ImageSourceChangedObs
-        {
-            get => _imageSourceChangedObs;
-            set
-            {
-                _imageSourceChangedSubscription?.Dispose();
-                _imageSourceChangedObs = value;
-                _imageSourceChangedSubscription = value.Subscribe(imageSource =>
-                {
-                    ImageSource = imageSource;
-                });
-
-            }
-        }
-
-        private IDisposable _headerTextChangedSubscription;
-        private IObservable<string> _subHeaderTextChangedObs;
-
-        public IObservable<string> HeaderTextChangedObs
-        {
-            get => _headerTextChangedObs;
-            set
-            {
-                _headerTextChangedSubscription?.Dispose();
-                _headerTextChangedObs = value;
-                _headerTextChangedSubscription = value.Subscribe(s =>
-                {
-                    HeaderText = s;
-                });
-            }
-        }
-
-        private IDisposable _subHeaderTextChangedSubscription;
-        private ImageSource _imageSource;
-        private string _headerText;
-        private string _subHeaderText;
-
-        public IObservable<string> SubHeaderTextChangedObs
-        {
-            get => _subHeaderTextChangedObs;
-            set
-            {
-                _subHeaderTextChangedSubscription?.Dispose();
-                _subHeaderTextChangedObs = value;
-                _subHeaderTextChangedSubscription = value.Subscribe(s =>
-                {
-                    SubHeaderText = s;
-                });
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
+    }                                           
 }
