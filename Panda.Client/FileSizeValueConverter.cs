03c750922fc857638c71fea923c9aa9845254bcd
Ty Smith <dts5004@gmail.com>
2018-01-05 19:26:48 -0800

    Turned file size conversion into a converter

---
+++ Panda.Client/FileSizeValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Humanizer;
+
+namespace Panda.Client
+{
+    public class FileSizeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+            var converted = System.Convert.ToUInt64(value);
+            return Humanizer.Bytes.ByteSize.FromBytes(converted).Humanize("#.#");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
