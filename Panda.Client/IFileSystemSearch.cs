aa6bae972a3af04f049fd20f1c4d92ffb73bba10
Ty Smith <dts5004@gmail.com>
2018-01-05 23:15:06 -0800

    Added an abstraction for file system searching

---
+++ Panda.Client/IFileSystemSearch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Panda.Client
+{
+    /// <summary>
+    ///     Represents something that is capable of searching the file system
+    /// </summary>
+    public interface IFileSystemSearch
+    {
+        /// <summary>
+        ///     Searches for files using the specified query.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        IObservable<FileInfo> Search(string query, CancellationToken cancellationToken);
+    }
+}
