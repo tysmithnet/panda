deca5949a61eb1925cdf724d75229d1c74890936
Ty Smith <dts5004@gmail.com>
2018-01-14 17:37:38 -0800

    Added hello world for log launcher

---
+++ Panda.LogLauncher/LogLauncher.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Panda.Client;
+
+namespace Panda.LogLauncher
+{
+    /// <summary>
+    /// Interaction logic for LogLauncher.xaml
+    /// </summary>
+    [Export(typeof(Launcher))]
+    public partial class LogLauncher : Launcher
+    {
+        public LogLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void LogLauncher_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel = new LogLauncherViewModel();
+            DataContext = ViewModel;
+        }
+
+        public LogLauncherViewModel ViewModel { get; set; }
+    }
+}
---
+++ Panda.LogLauncher/LogLauncherViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Panda.LogLauncher.Annotations;
+
+namespace Panda.LogLauncher
+{
+    public class LogLauncherViewModel : INotifyPropertyChanged
+    {
+        public string Greeting { get; set; } = "hello world";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
